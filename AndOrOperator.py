f65991bc18524b067def7deaa22332652b05f3b3
Manal BENCHRIF <72361536+ManalBenchrif@users.noreply.github.com>
2020-10-21 02:59:52 +0100

    Add files via upload

---
+++ AndOrOperator.py
@@ -0,0 +1,10 @@
+
+#****************Boolean operators**************
+#The "and" and "or" boolean operators allow building complex boolean expressions
+name=input("enter your name: ") #input is a function that ask for input
+age=int(input("enter your age: "))
+if name!="manal" or age!=21:
+    print("it's not me")
+if name=="manal" and age==21:
+    print("this is me ^_^")
+print("^_^ ^_^")
