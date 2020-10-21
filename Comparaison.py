9b0ba68544d157d935dca26a8a17b6689483a034
Manal BENCHRIF <72361536+ManalBenchrif@users.noreply.github.com>
2020-10-21 03:25:03 +0100

    Add files via upload

---
+++ Comparaison.py
@@ -0,0 +1,11 @@
+#Python uses boolean variables to evaluate conditions.
+#true id condition is true else false
+
+x=5   #assignment is done using a single equals operator "="
+print(x==5) # comparison between two variables is done using the double equals operator "==".
+print(x==4) 
+print(x>5) 
+print(x!=6) #The "not equals" operator is marked as "!=".
+
+
+
